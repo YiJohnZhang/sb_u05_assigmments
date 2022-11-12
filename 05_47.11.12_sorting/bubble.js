1377eff768cc771ffe00ac355fb9db6f03ed6dd5
YiJohnZhang <yijohnzhang@gmail.com>
2022-11-12 09:34:52 -0800

    dump break: pause `05_47.10.12`
    taking a dump

--- 05_47.11.12_sorting/bubble.js
+++ 05_47.11.12_sorting/bubble.js
@@ -1,3 +1,31 @@
-function bubbleSort() {}
+// assignment note: sort from min to max
+function bubbleSort(inputArray) {
+
+	for(let i=0; i < inputArray.length; i++){
+
+		let swapMade = false;
+
+		for(let j=1; j<inputArray.length-i; j++){
+
+			// bubble sort guarantees the last element is sorted
+
+			if(inputArray[j-1] > inputArray[j]){
+				// [inputArray[j-1], inputArray[j]] = [inputArray[j], inputArray[j-1]];
+				const temp = inputArray[j-1];
+				inputArray[j-1] = inputArray[j];
+				inputArray[j] = temp;
+				swapMade = true;
+			}
+
+		}
+
+		if(!swapMade)
+			return inputArray;
+
+	}
+
+	return inputArray;
+
+}
 
 module.exports = bubbleSort;
--- 05_47.11.12_sorting/merge.js
+++ 05_47.11.12_sorting/merge.js
@@ -1,5 +1,54 @@
-function merge() {}
+function merge(sortedArray1, sortedArray2) {
 
-function mergeSort() {}
+	let mergedArray = [];
+
+	if(sortedArray2[0] === undefined)
+		return sortedArray1;
+
+	function _merge(i = 0, j = 0){
+		// i is index for sortedArray1, j is index for sortedArray2
+
+		if(i === sortedArray1.length && j === sortedArray2.length)
+			return;
+
+		if(sortedArray1[i] <= sortedArray2[j] || j === sortedArray2.length){
+			//sortedArray1 element takes priority if duplicate values
+
+			mergedArray.push(sortedArray1[i]);
+			_merge(i+1, j);
+
+		}else{
+
+			mergedArray.push(sortedArray2[j]);
+			_merge(i, j+1);
+
+		}
+	
+	}
+
+	_merge();
+
+	return mergedArray;
+
+}
+
+function mergeSort(inputArray) {
+
+	// so I'm guessing the "breaking up" array is a recursive call stack?
+
+	// if(inputArray.length < 2)
+	// 	return merge(inputArray.slice(0, 1), inputArray.slice(1));
+	
+	// return mergeSort(inputArray.slice(0, Math.floor((inputArray.length-1)/2)));
+
+	// idea: mergeSort([2, 5, 1, 0, 3, 4]) => ???? => mergeSort([2], [5]), mergeSort([1], [0]), mergeSort([3], [4]) => merge([2,5], [0,1]), merge([3,4], undefined) => merge([0,1,2,5], [3,4])
+		//	base case: when the input array has length of 1.
+		//	however, the function only takes 1 parameter...
+
+	// do i have to simultaneously break up the or can I do bite-sized chunks i.e. sort[2, 5] = [2,5] then sort [1,0] = [0, 1] then merge [0, 1, 2, 5]?
+
+
+
+}
 
 module.exports = { merge, mergeSort};
